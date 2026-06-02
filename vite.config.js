76d36e3434a42d3154c906cdd4dab3d114994e58
Tonio Suessdorf <tonio.suessdorf@gmail.com>
2026-06-02 17:08:44 +0200

    Remove meta tag frame-ancestors none

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ const productionCspMeta = [
   "img-src 'self' data: https: blob:",
   "font-src 'self' data:",
   "connect-src 'self'",
-  "frame-ancestors 'none'",
   "base-uri 'self'",
   "form-action 'self'",
   "upgrade-insecure-requests",
